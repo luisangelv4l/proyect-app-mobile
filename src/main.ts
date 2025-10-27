6d0c3135827b93556b8cf8190591f9d48c7709e0
Luis Angel Valeriano <luisangelvaleriano.0927@gmail.com>
2025-10-26 19:18:29 -0500

    Agregado e inicializado Firebase

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({ projectId: "proyect-app-mobile", appId: "1:474188118427:web:d8d4a1420444754628493e", storageBucket: "proyect-app-mobile.firebasestorage.app", apiKey: "AIzaSyAZxSrC9L8HJn3xMXG1lRzFUmYKPjpuNfY", authDomain: "proyect-app-mobile.firebaseapp.com", messagingSenderId: "474188118427", measurementId: "G-RDK2QCYZ2K", projectNumber: "474188118427", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 });
